cfcc0acb62fbcb3a968d073e16dd31dadcc8db8f
amaranthineblu <amaranthineblu@yahoo.com>
2023-04-29 12:59:07 +0100

    score commit

--- script.js
+++ script.js
@@ -4,25 +4,20 @@ function computerPlay(){
     //random number from 1 - 3 when the page is loaded;
     const pick = Math.floor(Math.random() * 3 + 1);
     if (pick == 3 ){
-    //if the system picks the value 3, then the string "Scissors" is attached to it
-        return("scissors");   
+        return("scissors"); //if the system picks the value 3, then the string "Scissors" is attached to it
     }else if (pick == 2){      
-    //if the system picks the value 2, then the string "Rock" is attached to it
-        return("rock");
+        return("rock"); //if the system picks the value 2, then the string "Rock" is attached to it
     }else {
-    //if the system picks the value 1, then the string "Paper" is attached to it
-        return("paper");
+        return("paper"); //if the system picks the value 1, then the string "Paper" is attached to it
     }
 }
 computerPlay(); //Calling the function computerPlay
 
 // comparing the result of player and computer.
 function playRound(playerSelection, computerSelection ){
-    // if player value is the same as computer value.
-    if (playerSelection == computerSelection){
+    if (playerSelection == computerSelection){ // if player value is the same as computer value.
         return "win" //"You win!";
-    } else {
-        //If Player value isn't the same as computer value
+    } else { //If Player value isn't the same as computer value
         return "loose" //"You Lose!";
     }
 }
@@ -30,9 +25,10 @@ function playRound(playerSelection, computerSelection ){
 function game(){ //the game function
 	let playerScore = 0; //the computer score and the player score is set to zero
 	let computerScore = 0;
-
-	for (i= 0; i < 5; i++){
-		playerSelection = prompt('Please Type Something: You Have to choose between "PAPER","ROCK","SCISSORS"')
+	let gameCount = 0;
+	for (i = 0; i < 5; i++){
+		let round = i;
+		playerSelection = prompt(`Round ${round + 1} Please Type Something: You Have to choose between "PAPER","ROCK","SCISSORS"`)
 		.toUpperCase(); //you input something on the prompt
 		computerSelection = computerPlay().toUpperCase(); //the computer selection is from the computer play
 		score = playRound(playerSelection,computerSelection);
@@ -46,14 +42,11 @@ function game(){ //the game function
 	}
 
 	if (playerScore > computerScore){
-		playerScore++;
-		console.log(`You win! Score: ${playerScore} : ${computerScore}`)
+		console.log(`You win! Score: ${playerScore} : ${computerScore}`)//if playerscore is greater then you win
 	} else if (playerScore == computerScore){
-		console.log(`This is a draw ${playerScore} : ${computerScore}`);
+		console.log(`This is a draw ${playerScore} : ${computerScore}`); //if playerscore and computer score is equal, it is a draw
 	}else {
-		computerScore++;
-		console.log(`You Loose! Score: ${computerScore} to ${playerScore}`)      
-	}  
+		console.log(`You Loose! Score: ${computerScore} to ${playerScore}`);//if computer score is greater     
+	}  																																		//than playerscore then you loose
 }
-
 game();
